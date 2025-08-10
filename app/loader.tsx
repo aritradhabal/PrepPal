ba622028c09204428b679e9ce210ab4f96327dd7
Aritra Dhabal <aritra@mac.local>
2025-08-10 15:20:37 +0530

    init

---
+++ app/loader.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { LoaderThree } from "@/components/ui/loader";
+
+export function Loader() {
+  return <LoaderThree />;
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import Header from './_components/Header'
-import Hero from './_components/Hero'
-import Footer from './_components/Footer'
+import React from "react";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
+import Footer from "./_components/Footer";
+import { Loader } from "./loader";
 
 function page() {
   return (
-    <div className='max-h-svh flex flex-col'>
-
-      <Header/>
-      <Hero/>
-      <Footer/>
-
+    <div className="max-h-svh flex flex-col">
+      <Header />
+      <Hero />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
