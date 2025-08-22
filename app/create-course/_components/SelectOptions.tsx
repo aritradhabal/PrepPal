3aa2d5601f4e8d17e0a80cbe253d07e35f07025c
Aritra Dhabal <aritra@Mac.lan>
2025-08-22 18:56:36 +0530

    aritra

--- app/create-course/_components/SelectOptions.tsx
+++ app/create-course/_components/SelectOptions.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+"use client";
+import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 function SelectOptions() {
   return (
-    <div>SelectOptions</div>
-  )
+    <div className="max-w-svw my-7 mx-0 sm:mx-10 lg:my-8 grid grid-cols-1 sm:grid-cols-2 place-content-center gap-y-5">
+      <Select>
+        <SelectGroup className="flex flex-col sm:flex-row justify-center items-center">
+          <SelectLabel className="text-sm font-medium text-gray-950">
+            Difficulty Level
+          </SelectLabel>
+            <SelectTrigger className="w-[90svw] sm:w-xs">
+            <SelectValue placeholder="Choose an option" />
+            </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="easy">Easy</SelectItem>
+            <SelectItem value="medium">Medium</SelectItem>
+            <SelectItem value="hard">Hard</SelectItem>
+          </SelectContent>
+        </SelectGroup>
+      </Select>
+      <Select>
+        <SelectGroup className="flex flex-col sm:flex-row justify-center items-center">
+          <SelectLabel className="text-sm font-medium text-gray-950">
+            Course Duration
+          </SelectLabel>
+          <SelectTrigger className="w-[90svw] sm:w-xs">
+            <SelectValue placeholder="Choose an option" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="short">Short (&lt; 2 hr)</SelectItem>
+            <SelectItem value="normal">Medium (2–4 hr)</SelectItem>
+            <SelectItem value="lengthy">Long (&gt; 4 hr)</SelectItem>
+          </SelectContent>
+        </SelectGroup>
+      </Select>
+
+      <Select>
+        <SelectGroup className="flex flex-col sm:flex-row justify-center items-center">
+          <SelectLabel className="text-sm font-medium text-gray-950">
+            Include Videos
+          </SelectLabel>
+          <SelectTrigger className="w-[90svw] sm:w-xs">
+            <SelectValue placeholder="Choose an option" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="yes">Yes</SelectItem>
+            <SelectItem value="no">No</SelectItem>
+          </SelectContent>
+        </SelectGroup>
+      </Select>
+    </div>
+  );
 }
 
-export default SelectOptions
+export default SelectOptions;
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@ import Hero from "./_components/Hero";
 import Footer from "./_components/Footer";
 import { Loader } from "./loader";
 
+
 function page() {
   return (
     <div className="max-h-svh flex flex-col">
+      
       <Header />
       <Hero />
       <Footer />
